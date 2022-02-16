fcff98b78415a5930560e63f7c845e08c8647b8e
Alec Tang <55515112+atang816@users.noreply.github.com>
2022-02-15 20:57:04 -0800

    Added spring-initializer

---
+++ demo-initializer/src/test/java/com/example/demoinitializer/DemoInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoinitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
